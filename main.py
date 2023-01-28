4ab7ae1798e3a6c25fbdf43914475d2d55bc2772
KoshysDev <koshys.osu@gmail.com>
2023-01-28 11:41:26 +0200

    Query paremeters

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# uvicorn main:app --reload
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,6 +8,17 @@ app = FastAPI()
 def index():
     return {'data': {'home page'}}
 
-@app.get('/about')
-def about():
-    return {'data': {'about page'}}
+@app.get('/blog')
+def blog_list(limit = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from db loaded'}
+    else:
+        return {'data': f'{limit} blogs from db loaded'}
+
+@app.get('/blog/{blog_id}')
+def show_blog_by_id(blog_id: int):
+    return {'data': blog_id}
+
+@app.get('/blog/{blog_id}/comments')
+def blog_comments(blog_id):
+    return {'data': {'1', '2'}}
